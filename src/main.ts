6f3f1be76a724cc6750e0718f5e60b0d947616a3
Carlos Ferreira <carlos@especializati.com.br>
2023-06-16 11:40:58 -0300

    feat: work with environments

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,6 @@
-console.log('ops')
+import * as dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
+
+console.log('OpenAI API Key:', process.env.OPENAI_API_KEY);
