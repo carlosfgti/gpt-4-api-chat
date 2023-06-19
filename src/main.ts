c9114a090256d5d4d2626e316c90b9f33a418940
Carlos Ferreira <carlos@especializati.com.br>
2023-06-19 10:04:43 -0300

    feat: integration with api openai

--- src/main.ts
+++ src/main.ts
@@ -10,3 +10,22 @@ const configuration = new Configuration({
 });
 
 const openai = new OpenAIApi(configuration);
+
+const completion = async () => {
+  try {
+    const completion = await openai.createCompletion({
+      model: "gpt-3.5-turbo-16k",
+      prompt: "Hello world",
+    });
+    console.log(completion.data.choices[0].text);
+  } catch (error) {
+    if (error.response) {
+      console.log(error.response.status);
+      console.log(error.response.data);
+    } else {
+      console.log(error.message);
+    }
+  }
+}
+
+completion();
